da01e7c81cc87a7f9ed8da02c94e9226373e0a72
Bruce Melo <brucemelodf@gmail.com>
2023-04-22 16:10:01 -0300

    initial code.

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.0.6"
+    id("io.spring.dependency-management") version "1.1.0"
+}
+
+group = "com.github.brucemelo"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    runtimeOnly("com.h2database:h2")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.projectreactor:reactor-test")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/test/java/com/github/brucemelo/petadopt/PetAdoptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.brucemelo.petadopt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetAdoptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
